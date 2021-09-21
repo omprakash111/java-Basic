0d3b0f79d8f38c25c5f3d4d25c8e519048d52878
om <om160prakash@outlook.com>
2021-09-21 23:05:50 +0530

    find max ,min, bytes of int

---
+++ DataSizeRange.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class DataSizeRange
+{
+	 public static void main(String args[])
+	 {
+		 System.out.println("Min value of integer: "+Integer.MIN_VALUE);//-2147483648
+		 System.out.println("Min value of integer: "+Integer.MAX_VALUE);//2147483647
+		 System.out.println("Min value of integer: "+Integer.BYTES);//4
+	 }
+}
