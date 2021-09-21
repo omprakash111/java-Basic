42dd2fb4ad4948952120d89dfbb1b37a8c4e2fea
om <om160prakash@outlook.com>
2021-09-21 14:10:44 +0530

    read key from keyboard

---
+++ readInt.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+class readInt
+{
+	public static void main(String args[])
+	{
+		Scanner s= new Scanner(System.in);
+		
+		int a;
+		a=s.nextInt();
+		System.out.println(a);
+	}
+	
+	
+}
---
+++ readString.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+class readString
+{
+	public static void main(String args[])
+	{
+		Scanner s= new Scanner(System.in);
+		
+		String name;
+		name=s.nextLine();
+		System.out.println("Name is "+name);
+	}
+	
+	
+}
